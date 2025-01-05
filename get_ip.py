1d7cbe3b995021d8f2f5b70323f49bef51083021
Bhukubabu <143258170+bhukubabu@users.noreply.github.com>
2025-01-05 11:58:55 +0530

    Update get_ip.py

--- get_ip.py
+++ get_ip.py
@@ -1,9 +1,9 @@
-import requests
+import socket
 
 def get_public_ip():
     try:
-        response = requests.get("https://api.ipify.org?format=json")
-        public_ip = response.json().get("ip")
-        return public_ip
+        hostname = socket.gethostname()
+        local_ip = socket.gethostbyname(hostname)
+        return local_ip
     except requests.RequestException as e:
         return f"Error fetching public IP: {e}"
